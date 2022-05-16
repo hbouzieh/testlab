1a96e49453a8b8690dfb9822d8ff2562a2d07fa6
Ivan Rusakov <Ivan.Rusakov@microsoft.com>
2022-05-16 21:09:11 +0300

    UPD: More variables

--- deploy_vm.sh
+++ deploy_vm.sh
@@ -1,20 +1,25 @@
+# LAB VM
+# GENEREAL VARIABLES
 SUB=c8be08fd-adfc-4cd8-a24a-95960884bccb
 DC=01
-DCN=ne
-LOC=northeurope
-HOSTNAME=cpuutil-$DC-01
+LABN=77
+DCN=wu2
+LOC=westus2
+PREFIX=devops
+VMN=02
+ADM=adm-01
+PASS=""
+# VM OPTIONS
+RG=lab-$DC-$LABN
+HOSTNAME=$PREFIX-$DC-$VMN
 VMNAME=vmx-$DCN-$HOSTNAME
 IMAGE=MicrosoftWindowsServer:WindowsServer:2019-Datacenter-smalldisk:latest
 SIZE=Standard_D2as_v4
-ADM=adm-01
-PASS=""
+VIPSKU=Basic 
+STORSKU=Premium_LRS
 # DEFAULTS
-RG=lab-$DC-01
 NRG=lab-$DC-01
 SUBNET=/subscriptions/$SUB/resourceGroups/$NRG/providers/Microsoft.Network/virtualNetworks/net-$DC-01/Subnets/lan-$DC-01
 NSG=/subscriptions/$SUB/resourceGroups/$NRG/providers/Microsoft.Network/networkSecurityGroups/nsg-$DC-01
-VIPSKU=Basic 
-STORSKU=Premium_LRS
-
 	
 az vm create --resource-group $RG --image $IMAGE --size $SIZE --location $LOC --storage-sku $STORSKU --subnet $SUBNET --nsg $NSG --public-ip-sku $VIPSKU --name $VMNAME --public-ip-address-dns-name $VMNAME --computer-name $HOSTNAME --admin-username $ADM --admin-password $PASS
