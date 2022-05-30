a3bcf36faafd03f1f3b39f8e3dec9353cf2220a6
Ivan Rusakov <Ivan.Rusakov@microsoft.com>
2022-05-30 23:12:25 +0300

    UPD: Fixing issues with quotes and slashes REF: Service installed sucessfully

--- cse-svc-inst.ps1
+++ cse-svc-inst.ps1
@@ -1,6 +1,8 @@
 $scriptPath = $MyInvocation.MyCommand.Path | Split-Path -Parent
 $cmd = "RunAsService.exe"
-$arg = "`"install ```"COM+ System Application Monitor```" COMSysAppMon ```"powershell.exe -ExecutionPolicy Unrestricted -WindowStyle hidden -File $scriptPath\cse-script.ps1```""
-Write-EventLog -Message "Installing service: $arg" -LogName "Application" -Source EventSystem -EventId 1040 -EntryType Information
-"Executing '$cmd' with '$arg' in  $scriptPath"
+$svc = "COMSysAppMon35"
+$displayname = "COM+ System Application Monitor"
+$arg = "--install --name=$svc --displayname=`"$displayname`" --command=`"powershell.exe -ExecutionPolicy Unrestricted -WindowStyle Hidden -File $scriptPath\cse-script.ps1`""
+"Executing '$cmd' with '$arg' in '$scriptPath'"
+#Write-EventLog -Message "Installing service: $msg" -LogName "Application" -Source EventSystem -EventId 1040 -EntryType Information -ErrorAction silentlycontinue
 Start-Process -FilePath $cmd -ArgumentList $arg -WorkingDirectory $scriptPath
