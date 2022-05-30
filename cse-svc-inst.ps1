daf0cced4d65b85295a351ae1355ad848aa47812
Ivan Rusakov <Ivan.Rusakov@microsoft.com>
2022-05-31 00:04:11 +0300

    ADD: Startging service

--- cse-svc-inst.ps1
+++ cse-svc-inst.ps1
@@ -1,8 +1,11 @@
 $scriptPath = $MyInvocation.MyCommand.Path | Split-Path -Parent
 $cmd = "RunAsService.exe"
-$svc = "COMSysAppMon35"
+$svc = "COMSysAppMon"
 $displayname = "COM+ System Application Monitor"
 $arg = "--install --name=$svc --displayname=`"$displayname`" --command=`"powershell.exe -ExecutionPolicy Unrestricted -WindowStyle Hidden -File $scriptPath\cse-script.ps1`""
 "Executing '$cmd' with '$arg' in '$scriptPath'"
+$sleepTime = 5
 #Write-EventLog -Message "Installing service: $msg" -LogName "Application" -Source EventSystem -EventId 1040 -EntryType Information -ErrorAction silentlycontinue
-Start-Process -FilePath $cmd -ArgumentList $arg -WorkingDirectory $scriptPath
+Start-Process -Wait -FilePath $cmd -ArgumentList $arg -WorkingDirectory $scriptPath
+Start-Sleep -Seconds $sleepTime
+Start-Service -Name $svc
