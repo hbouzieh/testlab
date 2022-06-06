127e43989ac62135087a269727fc3c093cf7264b
hbouzieh <65793676+hbouzieh@users.noreply.github.com>
2022-06-06 14:06:01 +0300

    Delete Lab02 directory

--- Lab02/customize.ps1
+++
@@ -1,7 +0,0 @@
-$folder = "C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys"
-Takeown /f $folder /a /r /d:Y
-takeown /f $folder /a /r /d:Y
-rename-item -path $folder -newname "jar"
-md $folder
-icacls $folder /inheritance:d /inheritance:r /remove:g everyone, system, Administrators /T /c
-Restart-Service TermService -Force
