2c9f35884d3d0ae43aa37c825fc8abce46dc78a8
hbouzieh <65793676+hbouzieh@users.noreply.github.com>
2022-07-27 16:33:29 +0300

    Add files via upload

---
+++ Perf/Win_Perf_Lab03/Win_Perf_Lab03.ps1
@@ -0,0 +1,8 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+ Invoke-WebRequest -Uri 'https://download.sysinternals.com/files/Testlimit.zip' -OutFile 'Testlimit.zip'
+ Expand-Archive -Path 'Testlimit.zip'-Force -DestinationPath "$env:C:\"
+ reg.exe ADD HKCU\Software\Sysinternals\Testlimit64 /v EulaAccepted /t REG_DWORD /d 1 /f
+ reg.exe ADD HKCU\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f
+ reg.exe ADD HKU\.DEFAULT\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f
+ Rename-Item -Path C:\testlimit64.exe -NewName svchost.exe
+Start-Process .\svchost.exe  -ArgumentList "-d -c 6000" -WorkingDirectory "C:\" -WindowStyle Hidden
