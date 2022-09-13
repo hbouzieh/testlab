ecfb20c94e52879ef87fbfb3743b7e646011af0c
CiprianZ <82997964+CiprianZ@users.noreply.github.com>
2022-09-13 10:00:54 +0300

    Update lab12.ps1

--- Connectivity/lab12.ps1
+++ Connectivity/lab12.ps1
@@ -1,5 +1,10 @@
+net user locadm P@$$w0rd123 /add
+Net localgroup administrators locadm /add
 $folder = "C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys"
 md C:\ProgramData\Microsoft\Crypto\RSA\OLD
-icacls $folder /setowner "Administrators" /T
-mv C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys C:\ProgramData\Microsoft\Crypto\RSA\OLD
-Restart-Service TermService -Force
+icacls $folder /setowner "locadm" /T
+icacls $folder /inheritance:d
+mv C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys\* C:\ProgramData\Microsoft\Crypto\RSA\OLD
+del C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys\*
+icacls $folder /c /grant "Everyone:R"
+icacls $folder /remove locadm /t /c
