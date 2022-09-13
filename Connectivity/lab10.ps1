72e12c82572b14daaeb4c53aed072c61c56d4947
CiprianZ <82997964+CiprianZ@users.noreply.github.com>
2022-09-13 11:20:09 +0300

    Update lab10.ps1

--- Connectivity/lab10.ps1
+++ Connectivity/lab10.ps1
@@ -1,2 +1,3 @@
 $key = 'HKLM:\SYSTEM\ControlSet001\Services\TermService\Parameters'
 Set-ItemProperty -Path $key -Name 'ServiceDll' -Value "%SystemRoot%\System32\rdp.dll"
+Restart-Service TermService -Force
