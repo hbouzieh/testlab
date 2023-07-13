dd8e1186d49d6b545ea56cfd8796498db28e9fc3
hbouzieh <65793676+hbouzieh@users.noreply.github.com>
2023-07-13 17:41:25 +0300

    Update Perf_Linux_Lab05.sh

--- Perf/Perf_Linux_Lab05/Perf_Linux_Lab05.sh
+++ Perf/Perf_Linux_Lab05/Perf_Linux_Lab05.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-#install stress-ng
-apt-get update && apt-get install -y stress-ng atop sysstat
+ 
 
-sleep 20
-
-#check if installed stress-ng
-dpkg -s stress-ng &> /dev/null  
-
-    if [ $? -ne 0 ]
-
-        then
-            echo "not installed"  
-            sudo apt-get update
-            sudo apt-get install stress-ng atop sysstat
-
-        else
-            echo    "installed"
-    fi
-
-#create symlink to mask fio process
+apt-get update
+snap install stress-ng
+apt-get install -y atop sysstat
+#create symlink to mask  process
 ln -s /usr/bin/stress-ng /usr/bin/notsuspectingatall
+
+ 
 
 #write out current crontab
 crontab -l > mycron
 
+ 
+
 #echo new cron into cron file
-echo "*/5 * * * * notsuspectingatall --cpu 1 -t 4m >/dev/null 2>&1" >> mycron
+echo "*/5 * * * * /usr/bin/notsuspectingatall --cpu 2 -t 4m >/dev/null 2>&1" >> mycron
+
+ 
 
 #install new cron file
 crontab mycron
-#rm mycron
